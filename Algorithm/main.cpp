c0df82d32fd3a23ff96102bf2a1d4e41181af811
kenvin <summerHearts@163.com>
2017-03-17 16:52:44 +0800

    Initial Commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by Kenvin on 17/3/17.
+//  Copyright © 2017年 Kenvin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
